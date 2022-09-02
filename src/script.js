52d590f71faaef793f71bb6e3c0c20c3dce07916
ArthurJose2000 <arthur2000jose@gmail.com>
2022-09-02 00:32:38 -0300

    rotation

--- src/script.js
+++ src/script.js
@@ -60,9 +60,10 @@ var raycaster = new THREE.Raycaster();
 var clickMouse = new THREE.Vector2();
 var moveMouse = new THREE.Vector2();
 var draggable = new THREE.Object3D;
-var objects = [pinkTriangle, purpleTriangle, smallerRedTriangle, biggerRedTriangle, yellowTriangle, greenSquare, blueParalellgram]
+var objects = [pinkTriangle, purpleTriangle, smallerRedTriangle, biggerRedTriangle, yellowTriangle, greenSquare, blueParalellgram];
+var isPressingSpaceKey = false;
 
-window.addEventListener('click', (event) => {
+window.addEventListener('click', event => {
 
     if(draggable){
         draggable = null;
@@ -81,13 +82,28 @@ window.addEventListener('click', (event) => {
 	}
 })
 
-window.addEventListener('mousemove', (event) => {
+window.addEventListener('mousemove', event => {
+
     moveMouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
     moveMouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 })
 
+window.addEventListener('keydown', event => {
+
+    if (event.key == " "){
+        isPressingSpaceKey = true;
+    }
+
+})
+
+window.addEventListener('keyup', event => {
+
+    isPressingSpaceKey = false;
+
+})
+
 function dragObject() {
-    if(draggable != null){
+    if (draggable != null){
         raycaster.setFromCamera( moveMouse, camera );
 
         let intersects = raycaster.intersectObjects( objects );
@@ -95,9 +111,13 @@ function dragObject() {
 
             for ( let obj of intersects ) {
                 
-
-                draggable.position.x = obj.point.x;
-                draggable.position.y = obj.point.y;
+                if (isPressingSpaceKey){
+                    draggable.rotation.z += 0.02;
+                }
+                else{
+                    draggable.position.x = obj.point.x;
+                    draggable.position.y = obj.point.y;
+                }
             }
         }
     }    
